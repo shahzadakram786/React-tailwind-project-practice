5a9b8f19da54875497736a180c2cc1290d15bfd4
shahzadakram786 <alyakram786@gmail.com>
2024-06-01 15:26:08 +0500

    wokring on the testemonials section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import FeatureSection from "./components/FeatureSection";
+import Workflow from "./components/Workflow";
+import Pricing from "./components/Pricing";
+import Testemonials from "./components/Testemonials";
 
 const App = () => {
   return (
@@ -8,6 +12,10 @@ const App = () => {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection />
+        <Workflow />
+        <Pricing />
+        <Testemonials />
       </div>
     </>
   );
---
+++ src/components/Testemonials.jsx
@@ -0,0 +1,7 @@
+import { testimonials } from "../constants";
+
+const Testemonials = () => {
+  return <div></div>;
+};
+
+export default Testemonials;
