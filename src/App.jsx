06537b45b3c3adbed827941300857a97081b56ea
shahzadakram786 <alyakram786@gmail.com>
2024-06-01 13:30:43 +0500

    working on herosection

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </>
   );
 };
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import vedio1 from "../assets/video1.mp4";
+import vedio2 from "../assets/video2.mp4";
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide">
+        VirtualR build tools for developers
+      </h1>
+    </div>
+  );
+};
+
+export default HeroSection;
