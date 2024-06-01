45c9dcd3112d63ea906d5d09fd0c1b49c248e665
shahzadakram786 <alyakram786@gmail.com>
2024-06-01 12:18:53 +0500

    working from start and installing vite npm and tailwind

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="sticky top-0.z-50 py-3 backdrop-blur-lg border-b border-neutral-700/800">
+      <div className="container px-4 mx-auto relative text-sm">
+        <div className=""></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
