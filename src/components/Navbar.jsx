fb4d964e50e60c7b40d0d82f12d40b2e3f453e86
shahzadakram786 <alyakram786@gmail.com>
2024-06-03 14:51:13 +0500

    debugging succesfull

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0.z-50 py-3 backdrop-blur-lg border-b border-neutral-700/800">
+    <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/800">
       <div className="container px-4 mx-auto relative text-sm">
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
